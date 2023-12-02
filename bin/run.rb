fc983524eaac1cf76d3bf117e430d726551a6d5a
Peter Bailey <b33tle@gmail.com>
2023-12-02 12:13:51 -0400

    Dynamically import the per-day module

--- bin/run.rb
+++ bin/run.rb
@@ -1,8 +1,16 @@
 # bin/run.rb
 
 require_relative '../lib/my_cli'
-require_relative '../lib/day01'
-require_relative '../lib/day02'
+
+# Dynamically require all day modules
+(1..25).each do |day|
+  begin
+    require_relative "../lib/day#{day.to_s.rjust(2, '0')}"
+  rescue LoadError
+    # Silently ignore the error if the file/module doesn't exist
+  end
+end
+
 
 day = ARGV[0].to_i
 part = ARGV[1].to_i
